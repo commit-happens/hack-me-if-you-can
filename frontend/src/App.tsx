cd2db971fa0ad411b7672881259fa0632a3745cc
singha-cz <rene.kostka@singha.cz>
2025-11-05 20:20:58 +0100

    fix: update HTML language attribute to 'cs' and set document language in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import "./App.css";
-import useNavigate from "./hooks/useNavigate";
-import Game from "./pages/game";
-import Welcome from "./pages/welcome";
+import { useEffect, useState } from 'react';
+import './App.css';
+import useNavigate from './hooks/useNavigate';
+import Game from './pages/game';
+import Welcome from './pages/welcome';
 
 function App() {
   const { page, navigate } = useNavigate();
+  const [locale] = useState('cs');
 
   const pages = {
     welcome: <Welcome navigate={navigate} />,
     game: <Game navigate={navigate} page={page} />,
   };
+
+  useEffect(() => {
+    // Nastaví správný kód jazyka do <html lang="...">
+    document.documentElement.lang = locale;
+  }, [locale]);
 
   return pages[page];
 }
