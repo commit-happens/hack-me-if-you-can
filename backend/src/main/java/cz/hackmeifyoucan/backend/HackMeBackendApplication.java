3a54ec6a934e29033d4fc4c1b8ec78daf9a9494f
Eliska Kremenova <eliska.kremenova@allegro.com>
2025-11-05 21:08:47 +0100

    HMIYC-61 | update naming of main BE app

--- backend/src/main/java/cz/hackmeifyoucan/backend/HackMeBackendApplication.java
+++ backend/src/main/java/cz/hackmeifyoucan/backend/HackMeBackendApplication.java
@@ -1,13 +1,14 @@
+/*Main application entry point*/ 
 package cz.hackmeifyoucan.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class HackMeBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(HackMeBackendApplication.class, args);
 	}
 
 }
